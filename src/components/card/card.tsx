868dbe5cca4872ebadda0d6a995884646ca0fe3a
Ivan Fediakov <Ivan_Fediakov@epam.com>
2019-11-01 16:25:42 +0300

    feat(card): add className

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Card as CardType } from 'types/index';
 
-type Props = CardType;
+type Props = CardType & {
+    className: string;
+}
 
-export const Card = ({ text, imgPath, href }: Props) => {
+export const Card = ({ text, imgPath, className, href }: Props) => {
     return (
-        <a href={href} target="_blank" rel="noopener noreferrer">
+        <a className={className} href={href} target="_blank" rel="noopener noreferrer">
             <img src={imgPath} alt={text} />
             <h2>{text}</h2>
         </a>
